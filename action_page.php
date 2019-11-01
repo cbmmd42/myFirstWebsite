c65aaa5b80ece26ed9caec5d4fd862fd068c79b1
Michael Johnson <cbmmd42@yahoo.com>
2019-10-31 22:02:51 -0700

    slowly adding functionality to email submit button

--- action_page.php
+++ action_page.php
@@ -1,5 +1,13 @@
 <?php
 
-Hello World
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = $_POST['fname'];
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
 
 ?>
