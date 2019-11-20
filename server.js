5a28fccb5cc941d8de62a981b18c180f3acc1f53
Michael Johnson <cbmmd42@yahoo.com>
2019-11-20 14:03:51 -0800

    adding functionality to email form

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 #!/usr/bin/env nodejs
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'quizclient',
+    password: 'quizauth',
+    database: 'quizquestionsdb'
+});
+
+connection.connect(function(error) {
+    if(!!error){
+        console.log('Error connecting to database');
+    }else {
+        console.log('Connected to database');
+    }
+});
 
 
 app.use('/', express.static('.'));
+app.use(express.urlencoded());
+
+app.post('/submit-email', (req,res) => {
+	connection.query("SELECT questions.ID, questions.question FROM questions ", 
+    (error,results,fields) => {
+        if(!!error){
+            console.log('Error in the query');
+        }else {
+            console.log('Query successful');
+            res.send(results);
+            console.log(results);
+        }
+        
+    });
+});
 
 
 app.listen(8083,'localhost');
