ac27b80f88a61f38d3366e3ee83d0782221fe9ca
Michael Johnson <cbmmd42@yahoo.com>
2019-11-20 14:51:18 -0800

    test

--- server.js
+++ server.js
@@ -24,22 +24,9 @@ app.use(express.urlencoded());
 
 app.post('/submit-email', (req,res) => {
 	var subemail = req.body.email;
-<<<<<<< HEAD
-	connection.query("INSERT INTO `emails` (email) VALUES (?)", subemail.toString(), 
-    (error,results,fields) => {
-        if(!!error){
-            console.log('Error in the query');
-        }else {
-            console.log('Query successful');
-            res.send(results);
-            console.log(results);
-        }
-        
-    });
-=======
+
 	var sql = `INSERT INTO submittedemails ( id, email ) VALUES ( null, ${subemail.toString()} );`
 	console.log(sql);
->>>>>>> cabc6e15bfbdd7ed21a8fbf0fcf0b7093e43abe0
 });
 
 
